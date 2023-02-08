f61b35f71a602b4bd145b360b1981f0ce6433bb9
HarshAditya23 <34307686+HarshAditya23@users.noreply.github.com>
2023-02-08 11:02:29 +0530

    formatted

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const dotenv = require('dotenv');
 dotenv.config({
-    override: true
+	override: true
 });
 
 const PORT = process.env.PORT || 5000;
@@ -17,32 +17,34 @@ app.use(cors());
 
 mongoose.set("strictQuery", false);
 mongoose.connect(URI).then(() => {
-    console.log('Database connection successful.');
+	console.log('Database connection successful.');
 }).catch((err) => {
-    console.log('Database connection unsuccessful.');
+	console.log('Database connection unsuccessful.');
 });
 
 const dysdata = new mongoose.Schema({
-    title: String,
-    name: String,
-    contact: Number
+	title: String,
+	name: String,
+	contact: Number
 });
 
 const connect = mongoose.model('connect', dysdata);
 
 app.get('/', (req, res) => {
-    res.send("API is running..");
+	res.send("API is running..");
 });
 
 app.get('/dataresources/:title', (req, res) => {
-  dysdata.findOne({title: req.params.title}, (err, d_data) => {
-    if (err) {
-        return res.status(400).send(err);
-        }else
-    res.send(d_data);
-  });
+	dysdata.findOne({
+		title: req.params.title
+	}, (err, d_data) => {
+		if (err) {
+			return res.status(400).send(err);
+		} else
+			res.send(d_data);
+	});
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is up and running at http://127.0.0.1:${PORT}`);
+	console.log(`Server is up and running at http://127.0.0.1:${PORT}`);
 });
